ebaaa2cfbc5b47c2add2a9424debee388a0a5bac
Italo Manzzi <italomanzzi@outlook.com>
2025-08-06 19:21:39 -0300

    feat: add product list and product item components

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,16 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "d4lgxe9bm8juw.cloudfront.net",
+        pathname: "**",
+        //hostname: "d4lgxe9bm8juw.cloudfront.net",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,42 @@
+import ProductList from "@/components/common/product-list";
 import { Button } from "@/components/ui/button";
+import { db } from "@/db";
+import Image from "next/image";
 
-export default function Home() {
+const Home = async () => {
+  const products = await db.query.productTable.findMany({
+    with: {
+      variants: true,
+    },
+  });
+
   return (
-    <div className="container mx-auto p-4">
-      Testing
-      <Button className="p-4">Click</Button>
-      <Button variant="destructive">Destructive</Button>
+    <div className="space-y-6">
+      <div className="px-5">
+        <Image
+          src="/banner-01.png"
+          alt="Leve uma vida com estilo"
+          height={0}
+          width={0}
+          sizes="100dvw"
+          className="h-auto w-full"
+        />
+      </div>
+
+      <ProductList title="Mais vendidos" products={products} />
+
+      <div className="px-5">
+        <Image
+          src="/banner-02.png"
+          alt="Seja autêntico"
+          height={0}
+          width={0}
+          sizes="100vw"
+          className="h-auto w-full"
+        />
+      </div>
     </div>
   );
-}
+};
+
+export default Home;
