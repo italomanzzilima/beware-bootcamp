9d9fe6a6253b036ddc7e18b564084bae05792870
Italo Manzzi <italomanzzi@outlook.com>
2025-08-07 09:02:02 -0300

    feat: add category page

--- src/components/common/product-item.tsx
+++ src/components/common/product-item.tsx
@@ -1,15 +1,18 @@
 import { productTable, productVariantTable } from "@/db/schema";
 import { formatCentsToBRL } from "@/helpers/money";
+import { cn } from "@/lib/utils";
 import Image from "next/image";
 import Link from "next/link";
+import { text } from "stream/consumers";
 
 interface ProductItemProps {
   product: typeof productTable.$inferSelect & {
     variants: (typeof productVariantTable.$inferSelect)[];
   };
+  textContainerClassName?: string;
 }
 
-const ProductItem = ({ product }: ProductItemProps) => {
+const ProductItem = ({ product, textContainerClassName }: ProductItemProps) => {
   const firstVariant = product.variants[0];
   return (
     <Link href="/" className="flex flex-col gap-4">
@@ -18,9 +21,14 @@ const ProductItem = ({ product }: ProductItemProps) => {
         alt={firstVariant.name}
         height={150}
         width={150}
-        className="rounded-3xl"
+        className="h-auto w-full rounded-3xl"
       />
-      <div className="flex max-w-[150px] flex-col gap-1">
+      <div
+        className={cn(
+          "flex max-w-[200px] flex-col gap-1",
+          textContainerClassName,
+        )}
+      >
         <p className="truncate text-sm font-medium">{product.name}</p>
         <p className="text-muted-foreground truncate text-xs font-medium">
           {product.description}
